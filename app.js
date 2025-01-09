dbe2f984c2fc74b09dc03bd7513fd944158946d0
Sanika Almeida <sanikaalmeida@Sanikas-MacBook-Air.local>
2025-01-09 20:19:05 +0530

    chnages for render

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose=require("mongoose")
 const app=express()
 require("dotenv").config()
 
-const port=process.env.PORT
+const port=process.env.PORT || 8085
 app.use(express.json())
 app.use(cors({origin:"*"}))
 
